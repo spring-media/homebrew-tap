36faea8dbd6d90bbfd99bc00f20b1088dccdbe3b
goreleaserbot <goreleaser@carlosbecker.com>
2019-09-02 14:15:51 +0200

    Brew formula update for func version v0.0.3

--- func.rb
+++ func.rb
@@ -2,16 +2,16 @@
 class Func < Formula
   desc ""
   homepage ""
-  version "0.0.2"
+  version "0.0.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/spring-media/func/releases/download/v0.0.2/func_0.0.2_darwin_amd64.tar.gz"
-    sha256 "d8a364fd3c7e1c834b013e1ad5b7bc87e695cc80eba7d4072298bec3481037a3"
+    url "https://github.com/spring-media/func/releases/download/v0.0.3/func_0.0.3_darwin_amd64.tar.gz"
+    sha256 "325c78afe7f0199e2c77c82d5ab45926fd5ccf22bc1d65b1382fe5434eab1935"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/spring-media/func/releases/download/v0.0.2/func_0.0.2_linux_amd64.tar.gz"
-      sha256 "68594213e4dc6804aaef26192fcd04045b7fb1ca5fc19c48c17d10c1b9b7a902"
+      url "https://github.com/spring-media/func/releases/download/v0.0.3/func_0.0.3_linux_amd64.tar.gz"
+      sha256 "564603afaa454e74a404f0089289b934cd14137f9a5d9472c581a1debb2b0de8"
     end
   end
 
